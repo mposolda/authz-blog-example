b64b22fed4317e32f4ab69a216d5fda91cbafbac
mposolda <mposolda@gmail.com>
2016-12-07 10:05:21 +0100

    Step 4 - hardcoded resource

--- webapp/src/main/java/org/keycloak/example/Db.java
+++ webapp/src/main/java/org/keycloak/example/Db.java
@@ -3,6 +3,9 @@ package org.keycloak.example;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.stream.Collectors;
+
+import org.keycloak.AuthorizationContext;
 
 /**
  * @author <a href="mailto:mposolda@redhat.com">Marek Posolda</a>
@@ -11,8 +14,20 @@ public class Db {
 
     private List<Message> messages = new CopyOnWriteArrayList<>();
 
-    public List<Message> getMessages() {
-        return new ArrayList<>(messages);
+    public List<Message> getMessages(AuthorizationContext authzContext) {
+        List<Message> allMessages = new ArrayList<>(messages);
+
+        List<Message> allowedMessages = allMessages.stream().filter((Message m) -> {
+
+            String resourceName = "blogpost:" + m.getText().toLowerCase() + ":" + m.getUsername();
+            boolean hasPermission = authzContext.hasPermission(resourceName, "view-blogpost");
+
+            System.err.println("Resource name: " + resourceName + ", has permission: " + hasPermission);
+            return hasPermission;
+
+        }).collect(Collectors.toList());
+
+        return allowedMessages;
     }
 
     public void add(String message, String username) {
--- webapp/src/main/java/org/keycloak/example/ViewServlet.java
+++ webapp/src/main/java/org/keycloak/example/ViewServlet.java
@@ -9,7 +9,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.keycloak.KeycloakPrincipal;
-import org.keycloak.KeycloakSecurityContext;
 
 /**
  * @author <a href="mailto:mposolda@redhat.com">Marek Posolda</a>
@@ -36,7 +35,7 @@ public class ViewServlet extends HttpServlet {
 
         Db db = Utils.getDb(getServletContext());
 
-        for (Db.Message message : db.getMessages()) {
+        for (Db.Message message : db.getMessages(Utils.getAuthzContext(req))) {
             writer.println("<tr><td>" + message.getText() + "</td><td>" + message.getUsername() + "</td></tr>");
         }
 
